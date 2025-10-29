75c35667c3760fc3401efd7d18ab29fb0011cf6d
Refikls <nefedov.oleg.2004@gmail.com>
2025-10-29 15:03:59 +0300

    Добавлен логотип

--- components/navbar.py
+++ components/navbar.py
@@ -2,9 +2,12 @@ import dash_bootstrap_components as dbc
 import dash.html as html
 
 def create_navbar():
+    LOGO_PATH = '/assets/logo.png'
     navbar = dbc.NavbarSimple(
         brand=html.Span(
             [
+            html.Img(src=LOGO_PATH, height="30px", style={"marginRight": "10px"}),
+            
                 "Дашборд Студента"
             ]
         ),
